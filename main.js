7d27200ed11f3aea7c84770e1aaf5b9ee358eab3
Kion <kion@dashl.com>
2023-09-03 14:18:51 +0900

    remove functions

--- main.js
+++ main.js
@@ -11,24 +11,8 @@ const camera = new THREE.PerspectiveCamera(
   0.25,
   20
 );
+
 camera.position.set(-1.8, 0.6, 2.7);
-
-const addLights = () => {
-  const color = 0xffffff;
-  const directionalLightIntensity = 1;
-  const directionalLight = new THREE.DirectionalLight(color, directionalLightIntensity);
-  directionalLight.position.set(0, 10, 0);
-  directionalLight.target.position.set(-5, 0, 0);
-  scene.add(directionalLight);
-  scene.add(directionalLight.target);
-
-  const skyColor = 0xb1e1ff; // light blue
-  const groundColor = 0xb97a20; // brownish orange
-  const intensity = 10;
-  const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-  scene.add(light);
-}
-
 const render = () => {
   renderer.render(scene, camera);
 }
@@ -45,6 +29,7 @@ loader.load("DamagedHelmet.gltf", function (gltf) {
   scene.add(gltf.scene);
 });
 
+// Set up Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -53,13 +38,29 @@ renderer.toneMappingExposure = 1;
 document.body.appendChild(renderer.domElement);
 renderer.setAnimationLoop(render);
 
+// Set Up Contols
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener("change", render); // use if there is no animation loop
 controls.minDistance = 2;
 controls.maxDistance = 10;
 controls.target.set(0, 0, -0.2);
 controls.update();
-addLights();
+
+// Add directional light
+const color = 0xffffff;
+const directionalLightIntensity = 1;
+const directionalLight = new THREE.DirectionalLight(color, directionalLightIntensity);
+directionalLight.position.set(0, 10, 0);
+directionalLight.target.position.set(-5, 0, 0);
+scene.add(directionalLight);
+scene.add(directionalLight.target);
+
+// Add hemisphere light
+const skyColor = 0xb1e1ff; // light blue
+const groundColor = 0xb97a20; // brownish orange
+const intensity = 10;
+const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+scene.add(light);
+
 window.addEventListener("resize", onWindowResize);
 
 
